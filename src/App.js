1f388009b5fbe4b21b3cda350b3f54638f12494e
C-Skala <cjskala@gmail.com>
2022-08-13 19:45:36 -0400

    made user add option

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/Display Entries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
  
   const [entries, setEntries] = useState([{weight: 175, date: '11-3-2021'}, {weight: 180, date: '11-31-2021'}])
+
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry];
+    
+    setEntries(tempEntries);
+  }
  
   return (
     <div>
       <DisplayEntries parentEntries = {entries} />
+      <AddEntryForm addNewEntryProperty = {addNewEntry}/>
     </div>
   );
 }
